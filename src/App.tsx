3b8651e372a51be903cf445adb44c876f581ef2b
SailorMoonQ <dongwenquan3610@gmai.com>
2025-07-14 22:57:54 +0800

    feat(): add i18n

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
 import ProTip from './ProTip';
+import { useTranslation } from 'react-i18next';
 
 function Copyright() {
     return (
@@ -23,11 +24,13 @@ function Copyright() {
 }
 
 export default function App() {
+    const { t } = useTranslation();
+
     return (
         <Container maxWidth="sm">
             <Box sx={{ my: 4 }}>
                 <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
-                    Material UI Vite example in TypeScript
+                    {t('loading')}
                 </Typography>
                 <ProTip />
                 <Copyright />
